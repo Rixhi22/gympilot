10ad13f47514b806b9f59e1c770cf902d55be768
rishibala-m <20103091@hit.edu.in>
2026-02-15 18:17:49 +0530

    Fixed user auto deletion

--- create_admin.py
+++ create_admin.py
@@ -5,27 +5,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_r.settings')
 django.setup()
 
 from django.contrib.auth.models import User
-from django.contrib.sessions.models import Session
-from gym.models import Gym
 
-print("----- RESETTING DATABASE USERS -----")
-
-# Delete all sessions (important)
-Session.objects.all().delete()
-
-# Delete all users
-User.objects.all().delete()
-
-# Delete gym owners (if linked)
-Gym.objects.all().delete()
-
-print("Old users cleared")
-
-# Create fresh admin
 username = "admin"
 password = "admin123"
-email = "balarishi213@gmail.com"
 
-User.objects.create_superuser(username, email, password)
-
-print("New admin created successfully!")
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, password=password)
+    print("Admin created")
+else:
+    print("Admin already exists")
