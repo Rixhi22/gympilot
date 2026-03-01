b7f5c2a2a43a3ffc23185b612395d94ad603f063
rishibala-m <20103091@hit.edu.in>
2026-03-01 20:02:11 +0530

    added custom domain gympilot.online

--- project_r/settings.py
+++ project_r/settings.py
@@ -270,6 +270,7 @@ CSRF_COOKIE_SAMESITE = "Lax"
 # ======================================================
 # CSRF TRUSTED ORIGINS
 # ======================================================
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "http://localhost:8000"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://gympilot.online',
