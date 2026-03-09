74b4ed10f41946918c35a4b4a482f7d913b0ef71
Rixhi22 <90177161+Rixhi22@users.noreply.github.com>
2026-03-09 12:20:37 +0530

    Update admin.py

--- gym/admin.py
+++ gym/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import MembershipPlan, Member, Subscription, Trainer
+from .models import Gym, MembershipPlan, Member, Subscription, Trainer
 
+admin.site.register(Gym)
 admin.site.register(Member)
 admin.site.register(MembershipPlan)
 admin.site.register(Subscription)
 admin.site.register(Trainer)
+
